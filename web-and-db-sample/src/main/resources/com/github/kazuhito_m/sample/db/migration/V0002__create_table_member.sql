c91abb4ab0dde967c21873568ec21361fa85b1c5
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-08-29 01:06:15 +0900

    Flywayの設定を確定。サンプルのマイグレートSQLのひとつ目を追加。

---
+++ web-and-db-sample/src/main/resources/com/github/kazuhito_m/sample/db/migration/V0002__create_table_member.sql
@@ -0,0 +1,5 @@
+CREATE TABLE member (
+    member_id     INT UNSIGNED   PRIMARY KEY NOT NULL,
+    name          TEXT                       NOT NULL,
+    kana          TEXT                       NOT NULL
+);
