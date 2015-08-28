cd780073073e5c6fdf1c451a8d3c253748b91eeb
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-08-29 03:37:23 +0900

    ２個めのマイグレーションファイルを追加。

---
+++ web-and-db-sample/src/main/resources/com/github/kazuhito_m/sample/db/migration/V0003__add_initial_datas.sql
@@ -0,0 +1,4 @@
+insert into member(member_id, name, kana) values (1, 'Haljic','ハルジック');
+insert into member(member_id, name, kana) values (2, 'yukeeen', 'ユキーン');
+insert into member(member_id, name, kana) values (3, 'irof', 'イロフ');
+insert into member(member_id, name, kana) values (4, 'kazuhito_m', 'ナンデキタン');
