11d17e3077a06838bbf81afe089d3df8f8c2ceba
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-08-31 03:46:09 +0900

    環境をUbuntuベースに変更の途中経過まで。

--- environment/scripts/startup.sh
+++ environment/scripts/startup.sh
@@ -5,24 +5,24 @@
 # author : kazuhito_m
 
 # サービス開始(二回目以降はリスタート)
-service sshd restart
+service ssh restart
 
 # tomcat6 は途中で死ぬとゴミを残すので、予め「失敗しないカタチ」で削除実行。
-rm -f /var/run/tomcat6.pid
-rm -f /var/lock/subsys/tomcat6
-service tomcat6 restart
+rm -f /var/run/tomcat8.pid
+service tomcat8 restart
 
 # MySql再起動
-service mysqld restart
+service mysql restart
 
 # 日に二度Tomcatを上げ直すため、cron起動
-service crond restart
+# service cron restart
 
 # mysqlに外から入れるよう設定(二回目以降も権限系だけはふり直す)
-mysql --user=root --connect-expired-password -e "SET PASSWORD FOR root@localhost=PASSWORD('root');"
-mysql --user=root --password=kazuhito -e 'CREATE DATABASE IF NOT EXISTS sample;'
-mysql --user=root --password=kazuhito -e 'GRANT ALL PRIVILEGES ON *.* TO sample@"%.%.%.%" IDENTIFIED BY "sampleps";'
-mysql --user=root --password=kazuhito -e 'GRANT ALL PRIVILEGES ON *.* TO kazuhito@"localhost" IDENTIFIED BY "sampleps";'
+# mysql --user=root --connect-expired-password -e "SET PASSWORD FOR root@localhost=PASSWORD('root');"
+# 上の設定は「Dockerfileで担保済」のはず。
+mysql --user=root --password=root -e 'CREATE DATABASE IF NOT EXISTS sample;'
+mysql --user=root --password=root -e 'GRANT ALL PRIVILEGES ON *.* TO sample@"%.%.%.%" IDENTIFIED BY "sampleps";'
+mysql --user=root --password=root -e 'GRANT ALL PRIVILEGES ON *.* TO root@"localhost" IDENTIFIED BY "sampleps";'
 
 # デーモン用にエンドレス実行の何かを置く
 tail -f /dev/null
