426c9bf8700fe99d4cbe9a1a401893ce8542d822
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-08-30 21:52:30 +0900

    この例は本気でやってないよ！わざとひどいことにしてあるよ！をコメントにして表記。

--- web-and-db-sample/src/main/java/com/github/kazuhito_m/sample/servlet/Index.java
+++ web-and-db-sample/src/main/java/com/github/kazuhito_m/sample/servlet/Index.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 
+/**
+ * 「きょうびこんな実装は無いやろ…」という簡素な例。
+ */
 public class Index extends HttpServlet {
 
 
@@ -24,42 +27,29 @@ public class Index extends HttpServlet {
 
         try {
 
-
             DbAccesser db = new DbAccesser();
-
-            // データベースへのアクセス
             db.open();
 
-            // メンバーを取得
-            ResultSet rs = db.getResultSet("select * from member");
+            ResultSet rs = db.getResultSet("select * from member order by member_id");
 
-            // メンバー一覧表示用のテーブル
             String tableHTML = "<table border=1>";
             tableHTML += "<tr bgcolor=\"000080\"><td><font color=\"white\">メンバーID</font></td>"
                     + "<td><font color=\"white\">名前</font></td>"
                     + "<td><font color=\"white\">カナ</font></td>";
-
-            // 取得された各結果に対しての処理
             while (rs.next()) {
-
                 int id = rs.getInt("member_id");
                 String name = rs.getString("name");
                 String kana = rs.getString("kana");
 
-
-                // テーブル用HTMLを作成
-                tableHTML += "<tr><td align=\"right\">" + id + "</td>"
+               tableHTML += "<tr><td align=\"right\">" + id + "</td>"
                         + "<td>" + name + "</td><td>" + kana + "</td></tr>";
             }
-
             tableHTML += "</table>";
 
-            // データベースへのコネクションを閉じる
             db.close();
 
             resp.setContentType("text/html; charset=utf8");
             PrintWriter out = resp.getWriter();
-
             out.println("<html>");
             out.println("<head>");
             out.println("<title>member refalence</title>");
