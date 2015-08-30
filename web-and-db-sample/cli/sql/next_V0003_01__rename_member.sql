27c71b160c359898f0efc6bf87786bd5ffd9954b
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-08-30 22:00:31 +0900

    マイグレーションの例にストーリーを持たせる。

---
+++ web-and-db-sample/cli/sql/next_V0003_01__rename_member.sql
@@ -0,0 +1,3 @@
+insert into member(member_id, name, kana) values (4, 'kazuhito_m', 'ナンデキタン');
+
+update member set name = 'Haljik' where member_id = 1;
---
+++ web-and-db-sample/cli/sql/next_V0004__add_member.sql
@@ -0,0 +1,1 @@
+insert into member(member_id, name, kana) values (4, 'kazuhito_m', 'ナンデキタン');
