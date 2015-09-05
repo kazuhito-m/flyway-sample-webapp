2c72508d27952b1eae16f79578cc60465c0b69ca
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-09-05 20:53:40 +0900

    javaでマイグレーションする例を追加、コードアップ。デバッグは未。

--- web-and-db-sample/src/main/java/com/github/kazuhito_m/sample/db/migration/V0004__renumber_ids.java
+++ web-and-db-sample/src/main/java/com/github/kazuhito_m/sample/db/migration/V0004__renumber_ids.java
@@ -3,7 +3,11 @@ package com.github.kazuhito_m.sample.db.migration;
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * データの状態を見て「業務的・論理的に判断して更新」するようなマイグレーション。
@@ -14,17 +18,51 @@ public class V0004__renumber_ids implements JdbcMigration {
 
     @Override
     public void migrate(Connection connection) throws Exception {
-        try (Statement statement = connection.createStatement();) {
 
-            if ( statement.execute(sql) ) {
+        connection.setAutoCommit(false);  //オートコミットはオフ
 
-            }
-
-            return statement.getResultSet();
-
-
-
-            statement.executeUpdate("INSERT INTO HOGE VALUES (1, 'hoge')");
+        // リナンバーしたい…が、そのまますると被るので、一度わけわからんくらい飛ばす。
+        try (Statement statement = connection.createStatement()) {
+            String sql = "UPDATE member SET member_id = member_id + 10000;";
+            statement.executeUpdate(sql);
         }
+
+        // 名前に「特定の文字」を含むものだけを前に、それ意外は名前順に整理。
+        List<Integer> firstIds = new ArrayList<>();
+        try (Statement statement = connection.createStatement()) {
+
+            List<Integer> seccondIds = new ArrayList<>();
+
+            String sql = "SELECT member_id , kana FROM member ORDER BY kana";
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                // 値取得。
+                int id = rs.getInt("member_id");
+                String kana = rs.getString("kana");
+                // 特定の文字「イロフ」を含むなら…。
+                if (kana.contains("イロフ")) {
+                    firstIds.add(id);
+                } else {
+                    seccondIds.add(id);
+                }
+            }
+            // 最初のIDリストに合成する。
+            firstIds.addAll(seccondIds);
+        }
+
+        // IDの振り直し。
+        int i = 1;
+        String sql = "UPDATE member SET member_id = ? WHERE member_id = ?";
+        for (Integer id : firstIds) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, i++);
+                ps.setInt(2, id);
+                ps.executeUpdate();
+            }
+        }
+
+        connection.commit();
+
     }
 }
