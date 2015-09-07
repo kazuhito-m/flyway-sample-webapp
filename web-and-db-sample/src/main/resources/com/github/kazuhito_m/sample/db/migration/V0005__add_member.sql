ddf775aba3c56e83b3868bbe014e7cf7a7467520
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-09-07 23:35:37 +0900

    サンプルのマイグレーション中「サブクエリ風にインサート書いたやつ」が動かなかったため、ベタなダイレクトパスインサートに切り替え。

--- web-and-db-sample/src/main/resources/com/github/kazuhito_m/sample/db/migration/V0005__add_member.sql
+++ web-and-db-sample/src/main/resources/com/github/kazuhito_m/sample/db/migration/V0005__add_member.sql
@@ -1,1 +1,1 @@
-insert into member(member_id, name, kana) values ((SELECT MAX(member_id) + 1 FROM member), 'kazuhito_m', 'ナンデキタン');
+insert into member(member_id, name, kana) SELECT MAX(member_id) + 1 , 'kazuhito_m', 'ナンデキタン' FROM member;
